afe6dd62a48377cd81f8abe5455125b027342d0b
Daniel Zajork <dan@mr-pro-bot.local>
2022-04-29 13:33:07 -0400

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 alert(document.domain);
+alert(sessionStorage.getItem("accessToken"));
 
 @NgModule({
   declarations: [
